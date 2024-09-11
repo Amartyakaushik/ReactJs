11719d424da59cff0001289cf10dbca7fdaae127
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2024-09-11 19:07:19 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,70 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Paragraph from './Paragraph';
-import {SecondPara} from './Paragraph';
-import {ThirdPara} from './Paragraph';
+// import activity3 from './activity03';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// react-fragment is used to overcome the drawback of creation of div under div section
-// const myELement =<div> <h1>Heading 1</h1>  <p>This is paragraph tag</p></div>;
-const myELement = <><h1>Heading 1</h1>  <p>This is paragraph tag</p></>;
+const myELement = <>
 
-const myELement2 =<React.Fragment>
-  <table border={2}>
-          <tr>
-            <th>Roll N</th>
-            <th>name</th>
-            <th>Branch</th>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>Amartya</td>
-            <td>Cse</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Divyanshu</td>
-            <td>Cse</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Shagun</td>
-            <td>Cse</td>
-          </tr>
-          
-        </table>
-</React.Fragment>;
+<center >
+<table border={5} style={{'margin' : 40}}>
+            <tr>
+                <td>Company</td>
+                <td>Contact</td>
+                <td>kuch vi</td>
+                <td>Company</td>
+                <td>Contact</td>
+                <td>kuch vi</td>
+            </tr>
+            <tr>
+                <td>Google</td>
+                <td>+13 2345654345</td>
+                <td>kuch vi</td>
+                <td>Google</td>
+                <td>+13 2345654345</td>
+                <td>kuch vi</td>
+            </tr>
+        </table>,
+        <p> <h2>This is activity03</h2> \n This Dial widget example shows the following:
+
+A demonstration of a large value range combined with fine increment control.
+Setting UI strings before rendering
+Setting configuration attributes
+Construction-time event subscription allowing Dial to control an interactive UI
+Calling one of Dial's value change methods from the click of a link. <a>Hubble</a>
+Notice the Dial can traverse the entire 6,000+ pixels of the scene height, but by pulling the handle farther away from the Dial's center while rotating, the user can get 1 pixel movements, without strain. After the dial has focus, the following keys also opperate the Dial, arrow up/down/left/right, page up/down, home, end. The action of these keys can be controlled via Dial's configuration attributes. </p>
+        </center>
+</>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const root2 = ReactDOM.createRoot(document.getElementById('root2'));
-root.render(myELement)
-root2.render(myELement2);
+root.render(myELement);
 
-const root3 = ReactDOM.createRoot(document.getElementById('root3'));
-root3.render(
-  <React.StrictMode>
-    <Paragraph />
-    <SecondPara />
-    <ThirdPara />
-  </React.StrictMode>
-);
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// root.render(myELement);
-// const name = 'Divyanshu';
-// ReactDOM.createRoot(document.getElementById("root")).render(<React.Fragment> <h1>HELLO {name}</h1>  <p>This is paragraph tag</p></React.Fragment>);
-
-
-
-// ReactDOM.render(<div><h1>Heading 1</h1>  <p>This is paragraph tag</p></div>,
-//   <div> document.getElementById("root")</div>
-// );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
