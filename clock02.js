169c469d86c7552fb53311b83873705bff7a7d44
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2024-09-19 19:24:58 +0530

    Add files via upload

---
+++ clock02.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import Clock from './Clock';
+function useTime() {
+  const [time, setTime] = useState(() => new Date());
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+  return time;
+}
+// export default function clock02() {
+function clock02() {
+    const time = useTime();
+    const [color, setColor] = useState('lightcoral');
+    return (
+      <div>
+        <p>
+          Pick a color:{' '}
+          <select value={color} onChange={e => setColor(e.target.value)}>
+            <option value="lightcoral">lightcoral</option>
+            <option value="midnightblue">midnightblue</option>
+            <option value="rebeccapurple">rebeccapurple</option>
+          </select>
+        </p>
+        <Clock color={color} time={time.toLocaleTimeString()} />
+      </div>
+    );
+  }
+
+export default clock02;
