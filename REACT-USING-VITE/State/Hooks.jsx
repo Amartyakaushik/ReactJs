dcb8ef8ea5c23c98dab13909d1772c67a4c994b2
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2024-09-25 23:50:45 +0530

    Add files via upload

--- REACT-USING-VITE/State/Hooks.jsx
+++ REACT-USING-VITE/State/Hooks.jsx
@@ -1,1 +1,27 @@
+import React, { useState } from "react";
+function Hooks(){
+    // const returnValue = useState("Jolly");
+    // console.log(returnValue);
+    // const firstName = returnValue[0];
+    // const setName = returnValue[1];
 
+    //                    or
+    const [firstName, setName] = useState("Charchil");
+
+    function clickhandler(){
+        if(firstName == "Charchil"){
+            setName("Amartya");
+        }else{
+            setName("Charchil");
+        }
+    }
+
+    return(
+        <>
+            <h2>First Name is {firstName}</h2>
+            <button onClick={clickhandler}>Change Name</button>
+        </>
+    )
+}
+
+export default Hooks;
