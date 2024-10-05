1d734bd87c22b125e6298337f6cc98365212ea9d
Amartya <kamartya58@gmail.com>
2024-10-05 23:47:33 +0530

    adding

--- src/State/UpdateStateUsingObject.jsx
+++ src/State/UpdateStateUsingObject.jsx
@@ -1,35 +1,37 @@
-import React, {useState} from "react";
-const UpdateStateUsingObject = () =>{
-    const [user, setUser] = useState({
-        id : 1,
-        fName : "Amartya",
-        lName : "Kaushik",
-        email : "kamartya58@gmail.com",
-        age : 21
-    });
+import React, { useState } from "react";
+const UpdateStateUsingObject = () => {
+  const [user, setUser] = useState({
+    id: 1,
+    fName: "Amartya",
+    lName: "Kaushik",
+    email: "kamartya58@gmail.com",
+    age: 21,
+  });
 
-    const increaseAge = () => {
-        // setUser((prevState) => {
-        //     return {
-        //         ...prevState, age : prevState.age + 1
-        //     }
-        // })
-        setUser((prevState) =>({...prevState, age : prevState.age + 1}));
-    }
+  const increaseAge = () => {
+    // setUser((prevState) => {
+    //     return {
+    //         ...prevState, age : prevState.age + 1
+    //     }
+    // })
+    setUser((prevState) => ({ ...prevState, age: prevState.age + 1 }));
+  };
 
-    return (
-        <>
-        <center>
-            <h2>User Data</h2>
-            <p key={user.id}>Id: {user.id}</p>
-            <p>fName: {user.fName}</p>
-            <p>lName: {user.lName}</p>
-            <p>email: {user.email}</p>
-            <p>age: {user.age}</p>
-            <button onClick={increaseAge}>Increase Age</button>
-            </center>
-        </>
-    )
-}
+  // adding comment to push
+
+  return (
+    <>
+      <center>
+        <h2>User Data</h2>
+        <p key={user.id}>Id: {user.id}</p>
+        <p>fName: {user.fName}</p>
+        <p>lName: {user.lName}</p>
+        <p>email: {user.email}</p>
+        <p>age: {user.age}</p>
+        <button onClick={increaseAge}>Increase Age</button>
+      </center>
+    </>
+  );
+};
 
 export default UpdateStateUsingObject;
